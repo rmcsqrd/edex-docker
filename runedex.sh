b72e2d460e4d0ff91a367bce68518662bcf03721
mjames-upc <mjames@unidata.ucar.edu>
2018-09-26 21:01:07 -0600

    skip entrypoint

--- runedex.sh
+++ runedex.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+ulimit -n 1024
 set -e
 set -x
-regutil -s '$HOSTNAME' /hostname
+trap "echo TRAPed signal" HUP INT QUIT KILL TERM
+
+# EDEX
 edex setup
 /bin/bash /awips2/qpid/bin/qpid-wrapper
 /bin/bash /awips2/edex/bin/start.sh -noConsole ingest
 /bin/bash /awips2/edex/bin/start.sh -noConsole ingestGrib
+
+# LDM
+regutil -s '$HOSTNAME' /hostname
+ldmadmin mkqueue
+ldmadmin start
+
 # never exit
 while true; do sleep 10000; done
