8416ecffc14d7f78cabe2fd76acc126c333b679e
Rio McMahon <rmcmahon@ucar.edu>
2021-09-27 13:55:15 -0600

    added in conda install into edex_server container

---
+++ edex_server/etc/install_conda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# bash script to install gRPC stuff
+
+# install conda per https://phoenixnap.com/kb/how-to-install-anaconda-centos-7
+cd /tmp
+curl -O https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
+bash Anaconda3-2021.05-Linux-x86_64.sh -b
+
